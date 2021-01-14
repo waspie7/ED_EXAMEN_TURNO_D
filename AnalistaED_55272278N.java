4f6aa334e14139f5fb098eecdf967fee6b7437af
stephanie <stephpm748@gmail.com>
2021-01-14 13:00:49 +0100

    Se implementa codigo java sin javadoc DNI 55272278N

---
+++ AnalistaED_55272278N.java
@@ -0,0 +1,27 @@
+
+public class AnalistaED_55272278N {
+	static final double FACTOR_HORA_EXTRA = 1.2;
+	private String nombreCompleto, dni;
+	private double salarioBasePorHora = 10.56;
+	
+	public AnalistaED_55272278N(String nombre, String dni) {
+		this.nombreCompleto = nombre;
+		this.dni = dni;
+	}
+	
+	public double getSalarioEsteMes(int horasExtra) {
+		double cantidadExtra, salarioFinal;
+		cantidadExtra = horasExtra * this.salarioBasePorHora * FACTOR_HORA_EXTRA;
+		salarioFinal = this.salarioBasePorHora * 40 * 4 + cantidadExtra;
+		
+		return salarioFinal;
+	}
+	
+	public static void main(String[] args) {
+		AnalistaED_55272278N emp = new AnalistaED_55272278N("STEPHANIE PEREZ", "55272278N");
+		int horasExtra = 2;
+		
+		System.out.println("El salario de "+ emp.nombreCompleto + " este mes será de "+ emp.getSalarioEsteMes(horasExtra) + " euros, al hacer "+ horasExtra + " horas extra.");
+	}
+	
+}
